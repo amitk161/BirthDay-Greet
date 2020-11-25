a5a6201e27701749a9fd0b8c6c56b1db1487513d
amitkumar00756 <a.kamitkumar1920@gmail.com>
2020-11-25 19:31:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name = nameInput.editableText.toString()
+
+        val intent = Intent(this, BirthDayGreetingActivity::class.java)
+        intent.putExtra(BirthDayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+    }
+}
